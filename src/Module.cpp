d8cbd6b277b0f06d8e6ad1899ceae75ce90b3c4d
Paul Walker <paul@pwjw.com>
2023-01-18 16:48:40 -0500

    Sigh - windows

--- src/Module.cpp
+++ src/Module.cpp
@@ -13,6 +13,74 @@
 // @TODO: Newline in the tooltip text
 
 
+#if ARCH_WIN
+// Sigh. https://stackoverflow.com/questions/735126/are-there-alternate-implementations-of-gnu-getline-interface/735472#735472
+size_t getline(char **lineptr, size_t *n, FILE *stream)
+{
+    char *bufptr = NULL;
+    char *p = bufptr;
+    size_t size;
+    int c;
+
+    if (lineptr == NULL)
+    {
+        return -1;
+    }
+    if (stream == NULL)
+    {
+        return -1;
+    }
+    if (n == NULL)
+    {
+        return -1;
+    }
+    bufptr = *lineptr;
+    size = *n;
+
+    c = fgetc(stream);
+    if (c == EOF)
+    {
+        return -1;
+    }
+    if (bufptr == NULL)
+    {
+        bufptr = malloc(128);
+        if (bufptr == NULL)
+        {
+            return -1;
+        }
+        size = 128;
+    }
+    p = bufptr;
+    while (c != EOF)
+    {
+        if ((p - bufptr) > (size - 1))
+        {
+            size = size + 128;
+            bufptr = realloc(bufptr, size);
+            if (bufptr == NULL)
+            {
+                return -1;
+            }
+            p = bufptr + (size - 128);
+        }
+        *p++ = c;
+        if (c == '\n')
+        {
+            break;
+        }
+        c = fgetc(stream);
+    }
+
+    *p++ = '\0';
+    *lineptr = bufptr;
+    *n = size;
+
+    return p - bufptr - 1;
+}
+#endif
+
+
 #define MAX_POLY 16
 
 struct BufferedDrawFunctionWidget : virtual rack::FramebufferWidget
