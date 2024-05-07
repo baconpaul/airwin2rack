b6ad39e9daf1e4fbd53d9ce8b671b931d6617670
Paul Walker <paul@pwjw.com>
2024-05-06 20:10:06 -0400

    merge jatin branch / rebase

--- src-juce/AWConsolidatedEditor.cpp
+++ src-juce/AWConsolidatedEditor.cpp
@@ -180,12 +180,7 @@ void AWLookAndFeel::setLightTheme()
 
 juce::Font AWLookAndFeel::getPopupMenuFont()
 {
-    // Seems this font has different metrics on windows
-#if 0 // JUCE_WINDOWS
-    return juce::Font(jakartaSansMedium).withHeight(19);
-#else
     return juce::Font(jakartaSansMedium).withHeight(16);
-#endif
 }
 
 void AWLookAndFeel::drawPopupMenuBackgroundWithOptions(juce::Graphics &g, int width, int height,
