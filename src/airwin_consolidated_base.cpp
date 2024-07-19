8afc83b6e1a278a64a10ba31fa686a69ce94b5b9
Dan Green <danngreen1@gmail.com>
2024-07-19 12:04:40 -0700

    Apply clang-format

--- src/airwin_consolidated_base.cpp
+++ src/airwin_consolidated_base.cpp
@@ -1,11 +1,11 @@
 /*
-* AirwinConsolidated - an adaptation of the airwindows effect suite
-* for various open source clients
-*
-* This source released under the MIT License, found in ~/LICENSE.md.
-*
-* Copyright 2023 by the authors as described in the github transaction log
-*/
+ * AirwinConsolidated - an adaptation of the airwindows effect suite
+ * for various open source clients
+ *
+ * This source released under the MIT License, found in ~/LICENSE.md.
+ *
+ * Copyright 2023 by the authors as described in the github transaction log
+ */
 
 #include "airwin_consolidated_base.h"
 #include <string>
@@ -19,17 +19,17 @@ bool string2float(const char *txt, float &f)
     if (pEnd == txt)
     {
         return false;
-
-    } else if (v != INFINITY && v != -INFINITY && (v == HUGE_VALF || v == -HUGE_VALF))
+    }
+    else if (v != INFINITY && v != -INFINITY && (v == HUGE_VALF || v == -HUGE_VALF))
     {
         return false;
-
-    } else {
+    }
+    else
+    {
         f = v;
         return true;
     }
 }
-
 
 bool string2dBNorm(const char *txt, float &f)
 {
@@ -42,7 +42,7 @@ bool string2dBNorm(const char *txt, float &f)
 
     if (auto ok = string2float(txt, f))
     {
-        f = std::pow(10, f/20);
+        f = std::pow(10, f / 20);
         return true;
     }
     return false;
