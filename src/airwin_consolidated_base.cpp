096e0a748f5260050645d1f20aa28fe79a26626b
Dan Green <danngreen1@gmail.com>
2024-07-20 04:39:51 -0700

    Support compiling static library with exceptions disabled (#144)
    * condition CMRC on exceptions being present
    * string2float: Use throwing or non-throwing version, depending on exceptions support
    
    Allows complication for the 4ms target architecture for their upcoming device. Addresses #142

--- src/airwin_consolidated_base.cpp
+++ src/airwin_consolidated_base.cpp
@@ -1,21 +1,22 @@
 /*
-* AirwinConsolidated - an adaptation of the airwindows effect suite
-* for various open source clients
-*
-* This source released under the MIT License, found in ~/LICENSE.md.
-*
-* Copyright 2023 by the authors as described in the github transaction log
-*/
+ * AirwinConsolidated - an adaptation of the airwindows effect suite
+ * for various open source clients
+ *
+ * This source released under the MIT License, found in ~/LICENSE.md.
+ *
+ * Copyright 2023 by the authors as described in the github transaction log
+ */
 
 #include "airwin_consolidated_base.h"
 #include <string>
-#include <iostream>
 
 float AirwinConsolidatedBase::defaultSampleRate{0.f};
 
 bool string2float(const char *txt, float &f)
 {
-    try {
+#if defined(__EXCEPTIONS) || defined(__cpp_exceptions) || defined(_CPPUNWIND)
+    try
+    {
         float v = std::stof(txt);
         f = v;
         return true;
@@ -24,6 +25,23 @@ bool string2float(const char *txt, float &f)
     {
         return false;
     }
+#else
+    char *pEnd = nullptr;
+    float v = std::strtof(txt, &pEnd);
+    if (pEnd == txt)
+    {
+        return false;
+    }
+    else if (v != INFINITY && v != -INFINITY && (v == HUGE_VALF || v == -HUGE_VALF))
+    {
+        return false;
+    }
+    else
+    {
+        f = v;
+        return true;
+    }
+#endif
 }
 
 bool string2dBNorm(const char *txt, float &f)
@@ -35,12 +53,14 @@ bool string2dBNorm(const char *txt, float &f)
         return true;
     }
 
-    try {
+#if defined(__EXCEPTIONS) || defined(__cpp_exceptions) || defined(_CPPUNWIND)
+    try
+    {
         float v = std::stof(txt);
         // float2string ((float)(20.0 * log10 (value)), t, num);
         // so db = 20 log10(v)
         // v = 10^(db/20);
-        f = std::pow(10, v/20);
+        f = std::pow(10, v / 20);
         return true;
     }
     catch (const std::exception &e)
@@ -48,5 +68,12 @@ bool string2dBNorm(const char *txt, float &f)
 
         return false;
     }
+#else
+    if (auto ok = string2float(txt, f))
+    {
+        f = std::pow(10, f / 20);
+        return true;
+    }
+#endif
     return false;
 }
